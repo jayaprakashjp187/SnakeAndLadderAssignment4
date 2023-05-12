de04fb40a4292d52f7951a70e2629658b2b15683
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-12 20:25:22 +0530

    player rolling a dice to get number

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,6 +7,13 @@
             Console.WriteLine("welcome to the snake and ladder program!");
             int singlePlayerPosition = 0;
             Console.WriteLine("singleplayer position is now starting as zero");
+            int playerNumberOfRolls = 0; // number of times the dice was rolled
+            Random random = new Random();
+
+            int playerDiceRoll = random.Next(0, 6);
+            playerNumberOfRolls++;
+
+            Console.WriteLine("the player rolled a dice is   " + playerDiceRoll);
         }
     }
 }
