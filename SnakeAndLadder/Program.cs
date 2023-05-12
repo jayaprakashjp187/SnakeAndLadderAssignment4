9de7d08267330ef1a64d17c65743a326a794e242
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-12 20:11:45 +0530

    single player position

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the snake and ladder program!");
+            int singlePlayerPosition = 0;
+            Console.WriteLine("singleplayer position is now starting as zero");
+        }
+    }
+}
