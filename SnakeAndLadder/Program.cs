7a940e679831eca65ca118c87a50325acc920a21
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-13 13:44:59 +0530

    checking position no play,foreward or backward

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -5,15 +5,32 @@
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to the snake and ladder program!");
-            int singlePlayerPosition = 0;
+            int PlayerPosition = 0;
             Console.WriteLine("singleplayer position is now starting as zero");
             int playerNumberOfRolls = 0; // number of times the dice was rolled
             Random random = new Random();
 
-            int playerDiceRoll = random.Next(0, 6);
+            int playerDiceRoll = random.Next(1, 7);
             playerNumberOfRolls++;
 
             Console.WriteLine("the player rolled a dice is   " + playerDiceRoll);
+            int checkOptions=random.Next(0,3);
+            switch(checkOptions)
+            {
+                case 0:
+                    Console.WriteLine("No play");
+                    break;
+                case 1:
+                    PlayerPosition += playerDiceRoll;
+                    Console.WriteLine("player position moves a head");
+                    break;
+                case 2:
+                    PlayerPosition -= playerDiceRoll;
+                    Console.WriteLine("player position moves a behind");
+                    break;
+
+            }
+            Console.WriteLine("the position of player now  " + PlayerPosition);
         }
     }
 }
