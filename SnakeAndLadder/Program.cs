1ca636c9606825f01308582d070d91b1f6db51fb
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-13 14:03:05 +0530

    player reaching 100th position

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -9,28 +9,35 @@
             Console.WriteLine("singleplayer position is now starting as zero");
             int playerNumberOfRolls = 0; // number of times the dice was rolled
             Random random = new Random();
-
-            int playerDiceRoll = random.Next(1, 7);
-            playerNumberOfRolls++;
-
-            Console.WriteLine("the player rolled a dice is   " + playerDiceRoll);
-            int checkOptions=random.Next(0,3);
-            switch(checkOptions)
+            while (PlayerPosition < 100)
             {
-                case 0:
-                    Console.WriteLine("No play");
-                    break;
-                case 1:
-                    PlayerPosition += playerDiceRoll;
-                    Console.WriteLine("player position moves a head");
-                    break;
-                case 2:
-                    PlayerPosition -= playerDiceRoll;
-                    Console.WriteLine("player position moves a behind");
-                    break;
+                int playerDiceRoll = random.Next(1, 7);
+                playerNumberOfRolls++;
+
+                Console.WriteLine("the player rolled a dice is   " + playerDiceRoll);
+                int checkOptions = random.Next(0, 3);
+                switch (checkOptions)
+                {
+                    case 0:
+                        Console.WriteLine("No play");
+                        break;
+                    case 1:
+                        PlayerPosition += playerDiceRoll;
+                        Console.WriteLine("player position moves a head");
+                        break;
+                    case 2:
+                        PlayerPosition -= playerDiceRoll;
+                        Console.WriteLine("player position moves a behind");
+                        break;
+                }
+                if (PlayerPosition < 0)
+                {
+                    PlayerPosition = 0;
+                }
+
+                Console.WriteLine("the position of player now  " + PlayerPosition);
 
             }
-            Console.WriteLine("the position of player now  " + PlayerPosition);
         }
     }
 }
