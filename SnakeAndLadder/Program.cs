ab3a00bbb24fa7be4dcafe1c03aed93f31117fd4
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-13 14:10:48 +0530

    player position is now 100

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -34,6 +34,10 @@
                 {
                     PlayerPosition = 0;
                 }
+                if(PlayerPosition > 100)
+                {
+                    PlayerPosition = 100;
+                }
 
                 Console.WriteLine("the position of player now  " + PlayerPosition);
 
