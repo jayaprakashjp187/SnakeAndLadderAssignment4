6a476fff5acd32e3f02d421ceaaa5ede0dfc32d9
jayaprakashjp187 <130724450+jayaprakashjp187@users.noreply.github.com>
2023-05-13 14:28:13 +0530

    Update Program.cs

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -5,12 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to the snake and ladder program!");
-            int singlePlayerPosition = 0;
+            int PlayerPosition = 0;
             Console.WriteLine("singleplayer position is now starting as zero");
             int playerNumberOfRolls = 0; // number of times the dice was rolled
             Random random = new Random();
 
-            int playerDiceRoll = random.Next(0, 6);
+            int playerDiceRoll = random.Next(1, 7);
             playerNumberOfRolls++;
 
             Console.WriteLine("the player rolled a dice is   " + playerDiceRoll);
